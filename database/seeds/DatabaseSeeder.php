cc976aaaa040b1c1f3ce54c58f043aff0e9eb99f
dylan <527844046@qq.com>
2017-11-23 16:50:48 +0800

    adminlte

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,10 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminMenuTableSeeder::class);
         $this->call(AdminPermissionsTableSeeder::class);
         $this->call(AdminRolesTableSeeder::class);
-        $this->call(AdminRoleMenuTableSeeder::class);
-        $this->call(AdminRolePermissionsTableSeeder::class);
-        $this->call(AdminRoleUsersTableSeeder::class);
         $this->call(AdminUsersTableSeeder::class);
-        $this->call(AdminUserPermissionsTableSeeder::class);
     }
 }
